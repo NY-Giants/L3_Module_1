88d273e7b95eedfb66cc6fff858bde006a97e1e1
ShadowWolf <league@WTS15.attlocal.net>
2017-11-05 17:24:25 -0800

    gg
    Nearly done with moreandmoretests, probably staring the second part
    next class

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -10,40 +10,40 @@ import org.junit.Test;
 
 public class MoreAndMoreTests {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
+	@Test
+  public void MultTest() {
+
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+  }
+
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+
+	  }
+
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));
+	    assertFalse(isSquare(143));
+
+	  }
+
 //	@Test
 //	  public void CubeTest() {
 //
@@ -56,6 +56,44 @@ public class MoreAndMoreTests {
 //	    assertFalse(isCube(143));
 //
 //	  }
+
+	private boolean isSquare(int p) {
+		for(int q = 1; q <= p; q++) {
+			if(q*q == p) {
+				return true;
+			}
+			else {
+				
+			}
+			
+		}
+		
+		return false;
+	}
+
+	private boolean isPrime(int s) {
+		for(int i = 2; i < s; i++) {
+			int k = s%i;
+			
+			if(k == 0) {
+				return false;
+			}
+			else {
+				
+			}
+			
+		}
+		
+		return true;
+		
+	}
+
+	private String multiply(int i, int j) {
+		int k = i*j;
+		 String m = i + " x "+ j + " = " + k;
+		 
+		return m;
+	}
 
 
 
